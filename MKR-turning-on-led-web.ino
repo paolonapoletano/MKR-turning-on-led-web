05cebd584e7f340edab52e69bedb94f79de7d8e8
Paolo Napoletano <paolo.napoletano@unimib.it>
2019-03-31 23:51:06 +0200

    Init
    MKR

---
+++ MKR-turning-on-led-web.ino
@@ -0,0 +1,112 @@
+
+#include <SPI.h>
+#include <WiFi101.h>
+#include "secrets.h"
+IPAddress ip(149, 132, 182, 33);
+IPAddress subnet(255, 255, 255, 0);
+IPAddress dns(149, 132, 2, 3);
+IPAddress gateway(149, 132, 182, 1);
+char ssid[] = SECRET_SSID;        // your network SSID (name)
+char pass[] = SECRET_PASS;    // your network password (use for WPA, or use as key for WEP)
+
+int status = WL_IDLE_STATUS;
+WiFiServer server(80);
+
+int ledPin = LED_BUILTIN;   
+
+void setup() {
+  //Initialize serial and wait for port to open:
+  Serial.begin(115200);      // Initialize serial 
+  
+  // Connect or reconnect to WiFi
+  if(WiFi.status() != WL_CONNECTED){
+    Serial.print("Attempting to connect to SSID: ");
+    Serial.println(SECRET_SSID);
+       // WiFi.config(ip, dns, gateway, subnet);
+
+    while(WiFi.status() != WL_CONNECTED){
+      WiFi.begin(ssid, pass); // Connect to WPA/WPA2 network. Change this line if using open or WEP network
+      Serial.print(".");
+      delay(5000);     
+    } 
+    Serial.println("\nConnected");
+  }
+  server.begin();
+  // you're connected now, so print out the status:
+  printWifiStatus();
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+ 
+ listenForEthernetClients();
+
+}
+void listenForEthernetClients() {
+    WiFiClient client = server.available();   // listen for incoming clients
+
+ if (client) {                             // if you get a client,
+    Serial.println("new client");           // print a message out the serial port
+    String currentLine = "";                // make a String to hold incoming data from the client
+    while (client.connected()) {            // loop while the client's connected
+      if (client.available()) {             // if there's bytes to read from the client,
+        char c = client.read();             // read a byte, then
+        Serial.write(c);                    // print it out the serial monitor
+        if (c == '\n') {                    // if the byte is a newline character
+
+          // if the current line is blank, you got two newline characters in a row.
+          // that's the end of the client HTTP request, so send a response:
+          if (currentLine.length() == 0) {
+            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+            // and a content-type so the client knows what's coming, then a blank line:
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println();
+
+            // the content of the HTTP response follows the header:
+            client.print("Click <a href=\"/H\">here</a> turn the Built in LED on<br>");
+            client.print("Click <a href=\"/L\">here</a> turn the Built in LED off<br>");
+
+            // The HTTP response ends with another blank line:
+            client.println();
+            // break out of the while loop:
+            break;
+          }
+          else {      // if you got a newline, then clear currentLine:
+            currentLine = "";
+          }
+        }
+        else if (c != '\r') {    // if you got anything else but a carriage return character,
+          currentLine += c;      // add it to the end of the currentLine
+        }
+
+        // Check to see if the client request was "GET /H" or "GET /L":
+        if (currentLine.endsWith("GET /H")) {
+          digitalWrite(ledPin, HIGH);               // GET /H turns the LED on
+        }
+        if (currentLine.endsWith("GET /L")) {
+          digitalWrite(ledPin, LOW);                // GET /L turns the LED off
+        }
+      }
+    }
+    // close the connection:
+    client.stop();
+    Serial.println("client disonnected");
+  }
+}
+void printWifiStatus() {
+  // print the SSID of the network you're attached to:
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  // print your board's IP address:
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+
+  // print the received signal strength:
+  long rssi = WiFi.RSSI();
+  Serial.print("signal strength (RSSI):");
+  Serial.print(rssi);
+  Serial.println(" dBm");
+}
---
+++ secrets.h
@@ -0,0 +1,2 @@
+#define SECRET_SSID "IoTLabThingsU14"
+#define SECRET_PASS "L@b%I0T*Ui4!P@sS**0%Less"
